1b3303e8f717053eab028e6f9d4809b6f1823323
Daniel Morandini <danielmorandini@me.com>
2026-01-09 15:48:47 +0100

    Prepare for publication

--- mix.exs
+++ mix.exs
@@ -4,15 +4,31 @@ defmodule Flex.MixProject do
   def project do
     [
       app: :flex,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      name: "Flex",
+      source_url: "https://github.com/kim-company/flex"
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application, do: []
+
+  defp description do
+    "A high-level Elixir library for managing AWS ECS Fargate and managed instance tasks with support for running, monitoring, and controlling containerized workloads."
+  end
+
+  defp package do
+    [
+      maintainers: ["KIM Keep In Mind GmbH"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/kim-company/flex"}
+    ]
+  end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
